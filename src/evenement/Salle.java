4e21946bf63388f95a927922ce7e5981ea765ce5
SilasTeuliere <silas.teuliere@gmail.com>
2022-12-28 15:32:19 +0100

    Salle

--- src/evenement/Salle.java
+++ src/evenement/Salle.java
@@ -1,9 +1,9 @@
 package evenement;
 
 public enum Salle {
-	PETITESALLE("","",,,),
-	MOYENNESALLE(),
-	GRANDESALLE();
+	PETITESALLE("Tircrabe", "2 rue de la coquillette 31200 TOULOUSE", 50.00, 10, 20),
+	MOYENNESALLE("Saint Exupery", "7 rue du Jambonnot 31200 TOULOUSE", 75.00, 21, 50),
+	GRANDESALLE("Complèxe sportif Jean Bourdette", "87 Avenue Felix Armand 31200 TOULOUSE", 100.00, 51, 200);
 	private String nom;
 	private String adresse;
 	private double prix;
