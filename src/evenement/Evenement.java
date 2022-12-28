ee554d961d60c8deae208006cc5fc26746289177
SilasTeuliere <silas.teuliere@gmail.com>
2022-12-28 17:10:42 +0100

    Faisons les methode

--- src/evenement/Evenement.java
+++ src/evenement/Evenement.java
@@ -47,4 +47,5 @@ public class Evenement {
 		return description;
 	}
 	
+	
 }
--- src/evenement/InscritEven.java
+++ src/evenement/InscritEven.java
@@ -1,8 +1,42 @@
 package evenement;
 
+import membre.Membre;
+
 public class InscritEven {
-	private String date;
-	private Salle lieu;
-	private double budjetTotal;
+	private Membre membre;
+	private double budjetPrevisionnel;
+	private double budjetReel;
+	private FournitureInscrit[] fourniture;
+	
+	public InscritEven(Membre membre, double budjetPrevisionnel) {
+		super();
+		this.membre = membre;
+		this.budjetPrevisionnel = budjetPrevisionnel;
+	}
+
+	public double getBudjetReel() {
+		return budjetReel;
+	}
+
+	public void setBudjetReel(double budjetReel) {
+		this.budjetReel = budjetReel;
+	}
+
+	public FournitureInscrit[] getFourniture() {
+		return fourniture;
+	}
+
+	public void setFourniture(FournitureInscrit[] fourniture) {
+		this.fourniture = fourniture;
+	}
+
+	public Membre getMembre() {
+		return membre;
+	}
+
+	public double getBudjetPrevisionnel() {
+		return budjetPrevisionnel;
+	}
+	
 	
 }
