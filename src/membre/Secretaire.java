f8d6c16debf4683195383fdfb371bc1442832374
SilasTeuliere <silas.teuliere@gmail.com>
2022-12-29 11:22:09 +0100

    Secretaire 2

--- src/membre/Secretaire.java
+++ src/membre/Secretaire.java
@@ -28,4 +28,32 @@ public class Secretaire extends Membre {
 		return mails;
 	}
 	
+	public InscritEven[] ajoutInscrit(InscritEven inscrit) {
+		if(nbInscrits < nbInscritMaximum) {
+			inscrits[nbInscrits-1] = inscrit;
+			nbInscrits ++;
+		}
+		return inscrits;
+	}
+	
+	public InscritEven[] suppInscrit(InscritEven inscrit){
+		int i = 0;
+		while(!inscrit.getMembre().equals(inscrits[i].getMembre())) {
+			i++;
+		}
+		for(; i < nbInscrits - 1; i++) {
+			inscrits[i] = inscrits[i + 1];
+		}
+		nbInscrits--;
+		return inscrits;
+	}
+	
+	
+	public void afficherInscritCourant () {
+		for(int i  = 0; nbInscrits > i; i++) {
+			System.out.println("- Nom Prenom : "+ inscrits[i].getMembre().getNomPrenom() + ",  Fourniture apporté  : " + inscrits[i].getFourniture() );
+			System.out.println("\n");
+		}
+		System.out.println("-----------------");
+	}
 }
